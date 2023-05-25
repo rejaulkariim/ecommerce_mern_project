4ab183acfa27cdc6a3dec4334a119526a31516a3
rejaul-karim10 <rejalkarim66666@gmail.com>
2023-05-25 21:20:02 +0800

    added Express error Handling middleware

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Middlewares
 app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const isLoggedIn = (req, res, next) => {
   const login = true;
@@ -25,11 +26,23 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/api/user", isLoggedIn, (req, res) => {
+app.get("/api/users", isLoggedIn, (req, res) => {
   console.log(req.body.id);
   res.status(200).send({
     message: "user profile is returned",
   });
+});
+
+// client error handling
+app.use((req, res, next) => {
+  res.status(404).json({ message: "route not found" });
+  next();
+});
+
+// server error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Start the server and listen on port 5000
